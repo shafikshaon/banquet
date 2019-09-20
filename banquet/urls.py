5aecabd072e273e99dbc33d6939dac9bbd494f22
ShafikurRahmanShaon <shafikshaon@gmail.com>
2019-09-20 23:45:18 +0600

    * dashboard sample template add * gist app url register

--- banquet/urls.py
+++ banquet/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-urlpatterns = []
+urlpatterns = [
+    path('', include('gist.urls'))
+]
 
 if config('IS_DEVELOPMENT'):
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls)), ]
